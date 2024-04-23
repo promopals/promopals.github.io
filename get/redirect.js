b5097d35064e61943e2346d94c7544805918464b
promopals <166623980+promopals@users.noreply.github.com>
2024-04-23 11:59:24 +0200

    get redirect

---
+++ get/redirect.js
@@ -0,0 +1,11 @@
+function load() {
+  if (navigator.userAgent.match(/Android/i)) {
+    // use Android's redirect
+    document.location =
+      "play.google.com/store/apps/details?id=kodsnickerboa.promopalsbali";
+  } else {
+    // use iOS redirect
+    window.location.replace("https://apps.apple.com/app/id6480505108");
+  }
+}
+window.onload = load;
