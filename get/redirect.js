ab3029c38f979924b2a97e74295935921e66e2f8
promopals <166623980+promopals@users.noreply.github.com>
2024-04-23 12:10:48 +0200

    fix android redirect

--- get/redirect.js
+++ get/redirect.js
@@ -2,7 +2,7 @@ function load() {
   if (navigator.userAgent.match(/Android/i)) {
     // use Android's redirect
     document.location =
-      "play.google.com/store/apps/details?id=kodsnickerboa.promopalsbali";
+      "https://play.google.com/store/apps/details?id=kodsnickerboa.promopalsbali";
   } else {
     // use iOS redirect
     window.location.replace("https://apps.apple.com/app/id6480505108");
